e402a702abbd2600dfc6a0921ae498249b0b1f52
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2020-11-29 10:57:07 +0100

    Remove old (revoked) token

--- config/address.php
+++ config/address.php
@@ -4,7 +4,7 @@ return [
     'driver' => 'here',
 
     'here' => [
-        'token' => env('HERE_TOKEN', 'x_UUQ9sY8m-g0VCwlWty60SkK5yPg8Q3UQzT1Jwfn6I'),
+        'token' => env('HERE_TOKEN'),
     ],
 
     'locales' => null, // By default app.locale
